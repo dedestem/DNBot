e60decfcdec77720dbd15b40faa1f053d6ccf71e
David Bos <david.bos@outlook.com>
2024-09-24 16:36:45 +0200

    last mc backup

---
+++ Commands/Minecraft/LastBackup.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+module.exports = {
+	// Command configuration
+	data: new SlashCommandBuilder()
+		.setName('LastMCBackup')
+		.setDescription('Tells the latestbackup.txt'),
+
+	// Command execution code
+	async execute(interaction) {
+		// Path to the file you want to read
+        const filePath = path.join(os.homedir(), 'Desktop/DNBackend/MC/Backups/latest_backup.txt');
+
+		// Defer the reply to give time for file reading
+		await interaction.deferReply();
+
+		// Read the file asynchronously
+		fs.readFile(filePath, 'utf8', async (err, data) => {
+			if (err) {
+				// Handle file read errors
+				console.error(`Error reading file: ${err}`);
+				await interaction.editReply("Error reading the file.");
+				return;
+			}
+
+			// Send the file content in the reply
+			await interaction.editReply(`File contents:\n\`\`\`${data}\`\`\``);
+		});
+	},
+};
